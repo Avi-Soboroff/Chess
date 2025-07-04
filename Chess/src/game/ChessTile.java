98f3487e7c31f5939ba93f7b0b0a7d3d5d4325aa
avi <avi@mac.fios-router.home>
2025-07-04 13:57:30 -0400

    Second Commit

--- Chess/src/game/ChessTile.java
+++ Chess/src/game/ChessTile.java
@@ -1,5 +1,15 @@
 package game;
 
-public class ChessTile {
+import javax.swing.JButton;
 
+//each tile extends JButton which allows the user to interact with each tile by clicking
+public class ChessTile extends JButton {
+	private int row;
+	private int col;
+	
+	public ChessTile(int row, int col) {
+		this.row = row;
+		this.col = col;
+	}
+	
 }
--- Chess/src/gui/GameGUI.java
+++ Chess/src/gui/GameGUI.java
@@ -1,18 +1,45 @@
 package gui;
 
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 import game.ChessTile;
 
-//class the holds the game visuals(What pops up on the screen)
 public class GameGUI extends JFrame {
 	private final ChessTile[][] chessSquares = new ChessTile[8][8]; //gives us a full chess board that focuses on each individual square
 	private final ChessGame game = new ChessGame(); //instance of the game that will be shown on the screen
 	
+	
 	public GameGUI() {
 		setTitle("My Chess Game"); //gives the game a title
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //immediately exits the game once closed out
+		setLayout(new GridLayout(8,8));
+		setUpBoard(); 
+		pack(); //makes sure chess board fits in the window
+		setVisible(true);
+	}
+	
+	//sets up the full 8x8 chess board on the screen
+	private void setUpBoard() {
+		for (int row = 0; row < 8; row++) {
+			for (int col = 0; col < 8; col++) {
+				final int boardRow = row;
+				final int boardCol = col;
+				ChessTile newTile = new ChessTile(row, col);
+				newTile.addMouseListener(new MouseAdapter() { //allows user to click on each chess square
+					@Override
+					public void mouseClicked(MouseEvent e) {
+						
+					}
+				});
+				add(newTile);
+				chessSquares[row][col] = newTile; //adds tile to tile board
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
