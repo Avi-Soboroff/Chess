f84e0057b92177d57d63355694fd9c477ab939b2
avi <avi@mac.fios-router.home>
2025-07-10 09:15:48 -0400

    pawn update

--- Chess/src/pieces/Pawn.java
+++ Chess/src/pieces/Pawn.java
@@ -12,11 +12,34 @@ public class Pawn extends ChessPiece {
 	public boolean isValidMove(Position newPos, ChessPiece[][] board) {
 		//find out which color the pawn is first to determine the direction that it should go
 		int direction = pieceColor == PieceColor.WHITE ? -1 : 1;
-		//find the distance from starting position to new position
-		int numRows = (newPos.getRow()-position.getRow()) * direction; //pawn will either go forwards or backwards depending on the color
-		int numCols = newPos.getCol()-position.getCol();
-		//now check if it can do a one-square move
-		
+		//find starting and new positions
+		int startRow = position.getRow();
+		int startCol = position.getCol();
+		int endRow = newPos.getRow();
+		int endCol = newPos.getCol();
+		//make sure the requested move is in the board's bounds
+		if (endRow < 0 || endRow >= 8 || endCol < 0 || endRow >= 8) {
+			return false; //invalid if not in-bounds
+		}
+		int rowDiff = (endRow - startRow) * direction; //the distance the pawn wants to move
+		int colDiff = endCol - startCol;
+		//check if the pawn can make a one-square move; if the space ahead is occupied then the move is invalid
+		if (rowDiff == 1 && colDiff == 0 && board[endRow][endCol] == null) {
+			return true;
+		}
+		//check if the pawn can make a two-square move; this can only work if the pawn is in its starting position
+		if (((pieceColor == PieceColor.WHITE && startRow == 6) || (pieceColor == PieceColor.BLACK && startRow == 1)) 
+				&& rowDiff == 2 && colDiff == 0 && board[endRow][endCol] == null) {
+			int middleRow = startRow + direction;
+			if (board[middleRow][endCol] == null) {
+				return true;
+			}
+		}
+		//check if the pawn can make a diagonal capture. We use Math.abs to make sure it can do a diagonal capture in both directions
+		if (Math.abs(colDiff) == 1 && rowDiff == 1 && board[endRow][endCol] 
+				!= null && board[endRow][endCol].pieceColor != this.pieceColor) {
+			return true;
+		}
 		return false;
 	}
 }
